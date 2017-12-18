2cee68f5eb72a4b938ceab406bafea2c7e2d48b7
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-12-18 14:39:36 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ document.getElementById('btn').addEventListener("click", function(){
   fetch("https://randomuser.me/api/")
   .then(res => res.json())
   .then(json => something())
-
 })
 
 
 function something() {
-  document.getElementById('fullname').innerText(json.name.first)
-
+  document.getElementById('fullname').innerText = json.name.first
 }
